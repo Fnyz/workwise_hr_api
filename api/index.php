89ad321536fcdd686d4f223ccfd5ebae4d3ded5a
fnyz <jezabellebusico@gmail.com>
2024-07-17 22:47:43 +0800

    testing

--- api/index.php
+++ api/index.php
@@ -3,12 +3,20 @@
 // Load Laravel's autoload file
 require __DIR__ . '/../vendor/autoload.php';
 
+// Bootstrap Laravel application
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
+// Create an instance of the HTTP kernel
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::captureFrom()
-);
 
+// Create a request object (you need to decide what request data to use)
+$request = Illuminate\Http\Request::capture();
+
+// Handle the request through the kernel
+$response = $kernel->handle($request);
+
+// Send the response back to the client
 $response->send();
+
+// Terminate the application
 $kernel->terminate($request, $response);
